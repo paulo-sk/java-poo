441837b6f8a517791e7a607af9324287224e64c7
Paulo Estevao Santiago Teixeira <04702017110@dccbra.ifb.local>
2022-04-20 20:34:18 -0300

    exercicio 24

--- algoritomos-resumao/src/main/exercicio24/Main.java
+++ algoritomos-resumao/src/main/exercicio24/Main.java
@@ -1,10 +1,25 @@
 package main.exercicio24;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Main {
 
+	public static Scanner entrada = new Scanner(System.in);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		ArrayList<Integer> numeros = new ArrayList<Integer>();
+		System.out.println("Digite 3 numeros: ");
+		numeros.add(entrada.nextInt());
+		numeros.add(entrada.nextInt());
+		numeros.add(entrada.nextInt());
+		
+		Collections.sort(numeros);//ordem crescente por padrao
+		
+		System.out.println(numeros);
+		
 	}
 
 }
