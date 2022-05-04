46a5935c8741f86c68c7859016b2df2aec00be71
Paulo Estevao Santiago Teixeira <04702017110@dccbra.ifb.local>
2022-05-04 20:54:00 -0300

    Aula 4, horas

--- aulas/src/lista4_3/exercicio4/models/Horario.java
+++ aulas/src/lista4_3/exercicio4/models/Horario.java
@@ -1,15 +1,38 @@
 package lista4_3.exercicio4.models;
 
 public class Horario {
+	
 	private int hora;
 	private int minuto;
 	private int segundo;
 	
+	public Horario() {
+		
+	}
+	public Horario(int h) {
+		this.hora = h;
+	}
 	
+	public Horario(int h, int m) {
+		this(h);
+		this.minuto = m;
+	}
+	
+	public Horario(int h, int m, int s) {
+		this(h, m);
+		this.segundo = s;
+	}
+	
+	public Horario(Horario h) {
+		this.hora = h.hora;
+		this.minuto = h.minuto;
+		this.segundo = h.segundo;
+	}
 	
 	public int getHora() {
 		return hora;
 	}
+	
 	public void setHora(int hora) {
 		this.hora = hora;
 	}
@@ -26,7 +49,27 @@ public class Horario {
 		this.segundo = segundo;
 	}
 	
+	public void setHorario(int h, int m, int s) {
+		this.hora = h;
+		this.minuto = m;
+		this.segundo = s;
+	}
 	
 	
+	public void somarHoras() {
+		
+	}
+	
+	public void subtrairHoras() {
+			
+		}
+	
+	public int numSegundos() {
+		return 2;
+	}
+	
+	public void mostrarAMPM() {
+		
+	} 
 	
 }
